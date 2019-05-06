12f17e1666aa95b2e388ac3b01f20b1f2cb0395f
Kanters,Pim P.F.M <pim.kanters@student.fontys.nl>
2019-05-06 10:37:01 +0200

    Eerste react build push

---
+++ precache-manifest.42925db806ae9f989174f2951f89974a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ee8aefbda47a591ff0ec60ec575ac09",
+    "url": "/static/media/WhiteSockSmall.9ee8aefb.png"
+  },
+  {
+    "revision": "eb0544a8432b14a75b532ef5edcc6406",
+    "url": "/static/media/WhiteSockMedium.eb0544a8.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb541836d49cb22a59bb",
+    "url": "/static/js/main.f0b8273c.chunk.js"
+  },
+  {
+    "revision": "dfcfb8e72f9b7edd98de",
+    "url": "/static/js/2.f4d9a154.chunk.js"
+  },
+  {
+    "revision": "bb541836d49cb22a59bb",
+    "url": "/static/css/main.4913e7e9.chunk.css"
+  },
+  {
+    "revision": "ffe3b08b4f7a09aaa876ca065ab474e6",
+    "url": "/index.html"
+  }
+];
